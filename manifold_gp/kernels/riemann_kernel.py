5f4a32dcbdde0eb270bfeb72cbf16d310f6e2910
bernardo <bernardo.fichera@gmail.com>
2023-02-16 17:08:52 +0100

    Added modified kernel for posterior std; implemented random walk and symmetric Laplacian operators.

--- manifold_gp/kernels/riemann_kernel.py
+++ manifold_gp/kernels/riemann_kernel.py
@@ -18,20 +18,21 @@ from gpytorch.constraints import Positive
 
 from torch_geometric.utils import coalesce, scatter
 
-from typing import Optional, Union, Callable
+from typing import Optional, Union, Callable, Tuple
 from linear_operator import LinearOperator, settings, utils
 
 
 class RiemannKernel(gpytorch.kernels.Kernel):
     has_lengthscale = True
 
-    def __init__(self, nodes: torch.Tensor, neighbors: Optional[int] = 2, modes: Optional[int] = 10, **kwargs):
+    def __init__(self, nodes: torch.Tensor, neighbors: Optional[int] = 2, modes: Optional[int] = 10, support_kernel: Optional[gpytorch.kernels.Kernel] = gpytorch.kernels.RBFKernel(), **kwargs):
         super(RiemannKernel, self).__init__(**kwargs)
 
         # Hyperparameter
         self.nodes = nodes.unsqueeze(-1) if nodes.ndimension() == 1 else nodes
         self.neighbors = neighbors
         self.modes = modes
+        self.support_kernel = support_kernel
 
         # Heat kernel length parameter for Laplacian approximation
         # register the raw parameter
@@ -56,14 +57,37 @@ class RiemannKernel(gpytorch.kernels.Kernel):
     def spectral_density(self):
         raise NotImplementedError()
 
-    def forward(self, x1, x2, diag=False, **params):
-        covar = torch.mm(self.eigenfunctions(
-            x1).T, self.spectral_density().T*self.eigenfunctions(x2))
+    # def forward(self, x1, x2, diag=False, **params):
+    #     # covar = torch.mm(self.eigenfunctions(
+    #     #     x1).T, self.spectral_density().T*self.eigenfunctions(x2))
 
+    #     if diag:
+    #         if torch.equal(x1, x2):
+    #             return torch.sum(self.spectral_density().T*self.eigenfunctions(x1).pow(2), dim=0)
+    #         else:
+    #             return torch.sum(self.spectral_density().T*self.eigenfunctions(x1)*self.eigenfunctions(x2), dim=0)
+    #         # return covar.diag()  # temporary solution (make diagonal calculation more efficient)
+    #     else:
+    #         return torch.mm(self.eigenfunctions(x1).T, self.spectral_density().T*self.eigenfunctions(x2))
+    #         # return covar
+
+    def base(self, x1, x2, diag=False):
         if diag:
-            return covar.diag()  # temporary solution (make diagonal calculation more efficient)
+            if torch.equal(x1, x2):
+                return torch.sum(self.spectral_density().T*self.eigenfunctions(x1).pow(2), dim=0)
+            else:
+                return torch.sum(self.spectral_density().T*self.eigenfunctions(x1)*self.eigenfunctions(x2), dim=0)
         else:
-            return covar
+            return torch.mm(self.eigenfunctions(x1).T, self.spectral_density().T*self.eigenfunctions(x2))
+
+    def forward(self, x1, x2, diag=False, **params):
+        if diag:
+            if torch.equal(x1, x2):
+                return self.variance(x1)
+            else:
+                return self.base(x1, x2, diag=True) + self.support_kernel.forward(x1, x2, diag=True)*torch.sqrt(self.variance(x1) - self.base(x1, x1, diag=True))*torch.sqrt(self.variance(x2) - self.base(x2, x2, diag=True))
+        else:
+            return self.base(x1, x2) + self.support_kernel.forward(x1, x2)*torch.outer(torch.sqrt(self.variance(x1) - self.base(x1, x1, diag=True)), torch.sqrt(self.variance(x2) - self.base(x2, x2, diag=True)))
 
     @property
     def epsilon(self):
@@ -117,12 +141,18 @@ class RiemannKernel(gpytorch.kernels.Kernel):
 
     def solve_laplacian(self):
         # Lanczos
-        # t0 = time()
-        with gpytorch.settings.max_root_decomposition_size(self.modes):
-            self.eigenvalues, self.eigenvectors = self.laplacian(
-                operator=True).diagonalization(method="lanczos")
-        # t1 = time()
-        # print("Model 1: %.4g sec" % (t1 - t0))
+        t0 = time()
+        # with gpytorch.settings.max_root_decomposition_size(2*self.modes):
+        #     self.eigenvalues, self.eigenvectors = self.laplacian(
+        #         operator=True).diagonalization(method="lanczos")
+        #     self.eigenvalues[0] = 0
+        #     self.eigenvalues = self.eigenvalues[:self.modes]
+        #     self.eigenvectors = self.eigenvectors.to_dense()[:, :self.modes]
+        #     # self.eigenvectors = self.eigenvectors.to_dense()
+        # self.eigenvalues, self.eigenvectors = LaplacianSymmetric(self.values, self).diagonalization()
+        self.eigenvalues, self.eigenvectors = LaplacianRandomWalk(self.values, self).diagonalization()
+        t1 = time()
+        print("Model 1: %.4g sec" % (t1 - t0))
 
         # # Torch Sparse
         # t0 = time()
@@ -142,6 +172,11 @@ class RiemannKernel(gpytorch.kernels.Kernel):
         # t0 = time()
         # from scipy.sparse import coo_matrix
         # from scipy.sparse.linalg import eigs, eigsh
+        # val = -self.laplacian()
+        # val = torch.cat(
+        #     (val.repeat(2), torch.ones(self.nodes.shape[0]).to(self.nodes.device)), dim=0)
+        # idx = torch.cat((self.indices, torch.stack((self.indices[1, :], self.indices[0, :]), dim=0), torch.arange(
+        #     self.nodes.shape[0]).repeat(2, 1).to(self.nodes.device)), dim=1)
         # L = coo_matrix(
         #     (val.detach().cpu().numpy(), (idx[0, :].cpu().numpy(), idx[1, :].cpu().numpy())), shape=(self.nodes.shape[0], self.nodes.shape[0]))
         # T, V = eigsh(L, k=self.modes, which='SM')
@@ -155,6 +190,11 @@ class RiemannKernel(gpytorch.kernels.Kernel):
             x, self.neighbors)  # self.neighbors or fix it
         # return torch.sum(self.eigenvectors[indices].permute(2, 0, 1) * distances.div(-2*self.epsilon.square()).exp(), dim=2)
         return self.inverse_distance_weighting(distances, self.eigenvectors[indices].permute(2, 0, 1))
+
+    def variance(self, x):
+        distances, indices = self.knn.search(x, self.neighbors)
+
+        return torch.sum(self.spectral_density().T*self.inverse_distance_weighting(distances, self.eigenvectors[indices].pow(2).permute(2, 0, 1)), dim=0)
 
     def inverse_distance_weighting(self, d, y):
         u = torch.zeros(y.shape[0], y.shape[1]).to(d.device)
@@ -189,3 +229,88 @@ class LaplacianOperator(LinearOperator):
 
     def matmul(self, other: Union[torch.Tensor, "LinearOperator"]) -> Union[torch.Tensor, "LinearOperator"]:
         return self._matmul(other)
+
+
+class LaplacianSymmetric(LinearOperator):
+    def __init__(self, values, kernel):
+        super(LaplacianSymmetric, self).__init__(values, kernel=kernel)
+
+        self.values = values.div(-2*kernel.epsilon.square()).exp().squeeze()
+        self.indices = self._kwargs['kernel'].indices
+
+        # Diffusion Maps Normalization
+        deg = torch.zeros(self._kwargs['kernel'].nodes.shape[0]).to(self._kwargs['kernel'].nodes.device).scatter_add_(
+            0, self.indices[0, :], self.values).scatter_add_(0, self.indices[1, :], self.values)
+        self.values = self.values.div(deg[self.indices[0, :]]*deg[self.indices[1, :]])
+
+        # Symmetric Laplacian Normalization
+        deg = torch.zeros(self._kwargs['kernel'].nodes.shape[0]).to(self._kwargs['kernel'].nodes.device).scatter_add_(
+            0, self.indices[0, :], self.values).scatter_add(0, self.indices[1, :], self.values).sqrt()
+        self.values.div_(deg[self.indices[0, :]]*deg[self.indices[1, :]])
+
+    def _matmul(self, x):
+        # .div(self._kwargs['kernel'].epsilon.square().squeeze())
+        return x.index_add(0, self.indices[0, :], self.values.view(-1, 1) * x[self.indices[1, :]], alpha=-1).index_add(0, self.indices[1, :], self.values.view(-1, 1)*x[self.indices[0, :]], alpha=-1)
+
+    def _size(self):
+        dim = self._kwargs['kernel'].nodes.shape[0]
+        return torch.Size([dim, dim])
+
+    def _transpose_nonbatch(self):
+        return self
+
+    def evaluate_kernel(self):
+        return self
+
+    def matmul(self, other: Union[torch.Tensor, "LinearOperator"]) -> Union[torch.Tensor, "LinearOperator"]:
+        return self._matmul(other)
+
+    def diagonalization(self) -> Tuple[torch.Tensor, torch.Tensor]:
+        with gpytorch.settings.max_root_decomposition_size(3*self._kwargs['kernel'].modes):
+            evals, evecs = super().diagonalization(method="lanczos")
+            # evecs = evecs.to_dense()
+            # self.eigenvalues[0] = 0
+            evals = evals[:self._kwargs['kernel'].modes]
+            evecs = evecs.to_dense()[:, :self._kwargs['kernel'].modes]
+
+        return evals, evecs
+
+
+class LaplacianRandomWalk(LinearOperator):
+    def __init__(self, values, kernel):
+        super(LaplacianRandomWalk, self).__init__(values, kernel=kernel)
+
+        self.values = values.div(-2*kernel.epsilon.square()).exp().squeeze().repeat(2)
+        self.indices = torch.cat((self._kwargs['kernel'].indices, torch.stack(
+            (self._kwargs['kernel'].indices[1, :], self._kwargs['kernel'].indices[0, :]), dim=0)), dim=1)
+
+        # Diffusion Maps Normalization
+        deg = torch.zeros(self._kwargs['kernel'].nodes.shape[0]).to(
+            self._kwargs['kernel'].nodes.device).scatter_add_(0, self.indices[0, :], self.values)
+        self.values = self.values.div(deg[self.indices[0, :]]*deg[self.indices[1, :]])
+
+        # Random Walk Normalization
+        self.degree = torch.zeros(self._kwargs['kernel'].nodes.shape[0]).to(
+            self._kwargs['kernel'].nodes.device).scatter_add_(0, self.indices[0, :], self.values)
+        self.values.div_(self.degree[self.indices[0, :]])
+
+    def _matmul(self, x):
+        return x.index_add(0, self.indices[0, :], self.values.view(-1, 1) * x[self.indices[1, :]], alpha=-1)
+
+    def _size(self):
+        dim = self._kwargs['kernel'].nodes.shape[0]
+        return torch.Size([dim, dim])
+
+    def _transpose_nonbatch(self):
+        return self
+
+    def evaluate_kernel(self):
+        return self
+
+    def matmul(self, other: Union[torch.Tensor, "LinearOperator"]) -> Union[torch.Tensor, "LinearOperator"]:
+        return self._matmul(other)
+
+    def diagonalization(self) -> Tuple[torch.Tensor, torch.Tensor]:
+        evals, evecs = LaplacianSymmetric(self._args[0], self._kwargs['kernel']).diagonalization()
+
+        return evals, evecs.div_(self.degree.sqrt().view(-1, 1))
