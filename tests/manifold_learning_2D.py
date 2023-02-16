95505757453afda62adcca6bca52f5abe4547a5d
bernardo <bernardo.fichera@gmail.com>
2023-02-16 17:11:49 +0100

    Adapted scripts to new kernel structure.

--- tests/manifold_learning_2D.py
+++ tests/manifold_learning_2D.py
@@ -36,15 +36,13 @@ function_noise = 0.0
 noisy_y = sampled_y + function_noise * torch.randn(m).to(device)
 
 # Initialize kernel
-nu = 3
+nu = 2
 neighbors = 50
 modes = 100
-kernel = gpytorch.kernels.ScaleKernel(RiemannMaternKernel(
-    nu=nu, nodes=noisy_x, neighbors=neighbors, modes=modes))
+kernel = gpytorch.kernels.ScaleKernel(RiemannMaternKernel(nu=nu, nodes=noisy_x, neighbors=neighbors, modes=modes))
 
 # Initialize likelihood and model
-likelihood = gpytorch.likelihoods.GaussianLikelihood(
-    noise_constraint=gpytorch.constraints.GreaterThan(1e-8))
+likelihood = gpytorch.likelihoods.GaussianLikelihood(noise_constraint=gpytorch.constraints.GreaterThan(1e-8))
 model = RiemannGP(noisy_x, noisy_y, likelihood, kernel).to(device)
 
 # Model Hyperparameters
@@ -68,7 +66,7 @@ model.eval()
 
 with torch.no_grad(), gpytorch.settings.fast_pred_var():
     # High resolution mesh
-    # test_x, faces, test_y = groundtruth_from_mesh(
+    # sampled_x, faces, test_y = groundtruth_from_mesh(
     #     files('manifold_gp.data').joinpath('dragon100k.msh'))
 
     # GP posteriors
@@ -77,74 +75,74 @@ with torch.no_grad(), gpytorch.settings.fast_pred_var():
     std = preds.stddev.cpu().numpy()
     posterior_sample = preds.sample().cpu().numpy()
 
+    # Prior Variance
+    var = kernel.base_kernel.variance(noisy_x).cpu().numpy()
+
     # Kernel evaluation
     point = 3405  # 67757 in 100k mesh
-    kernel_eval = kernel(noisy_x[point, :].unsqueeze(
-        0), noisy_x).evaluate().squeeze().cpu().numpy()
+    kernel_eval = kernel(noisy_x[point, :].unsqueeze(0), noisy_x).evaluate().squeeze().cpu().numpy()
 
     # Eigenfunctions and Eigenvectors
-    mode = 50
+    mode = 1
     eigenvector = kernel.base_kernel.eigenvectors[:, mode].cpu().numpy()
     eigfunctions = kernel.base_kernel.eigenfunctions(noisy_x).cpu().numpy()
 
     # Bring data to numpy
-    test_x = sampled_x.cpu().numpy()
-    test_y = sampled_y.cpu().numpy()
+    sampled_x = sampled_x.cpu().numpy()
+    sampled_y = sampled_y.cpu().numpy()
 
 v_options = {'mode': 'sphere', 'scale_factor': 3e-3, 'color': (0, 0, 0)}
-
 # Ground Truth
-mlab.figure('Groud Truth', fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
-mlab.triangular_mesh(test_x[:, 0], test_x[:, 1],
-                     test_x[:, 2], faces, scalars=test_y)
+mlab.figure('Groud Truth', size=(1920, 1360), fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
+mlab.triangular_mesh(sampled_x[:, 0], sampled_x[:, 1], sampled_x[:, 2], faces, scalars=sampled_y)
 mlab.colorbar(orientation='vertical')
+mlab.view(0.0, 180.0, 0.5139171204775793)
 
 # Mean
-mlab.figure('Mean', fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
-mlab.triangular_mesh(test_x[:, 0], test_x[:, 1],
-                     test_x[:, 2], faces, scalars=mean)
+mlab.figure('Mean', size=(1920, 1360), fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
+mlab.triangular_mesh(sampled_x[:, 0], sampled_x[:, 1], sampled_x[:, 2], faces, scalars=mean)
 mlab.colorbar(orientation='vertical')
+mlab.view(0.0, 180.0, 0.5139171204775793)
 
 # Standard Deviation
-mlab.figure('Standard Deviation', fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
-mlab.triangular_mesh(test_x[:, 0], test_x[:, 1],
-                     test_x[:, 2], faces, scalars=std)
+mlab.figure('Standard Deviation', size=(1920, 1360), fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
+mlab.triangular_mesh(sampled_x[:, 0], sampled_x[:, 1], sampled_x[:, 2], faces, scalars=std)
 mlab.colorbar(orientation='vertical')
+mlab.view(0.0, 180.0, 0.5139171204775793)
+
+# Prior Variance
+mlab.figure('Prior Variance', size=(1920, 1360), fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
+mlab.triangular_mesh(sampled_x[:, 0], sampled_x[:, 1], sampled_x[:, 2], faces, scalars=var)
+mlab.colorbar(orientation='vertical')
+mlab.view(0.0, 180.0, 0.5139171204775793)
 
 # One Posterior Sample
-mlab.figure('One Posterior Sample', fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
-mlab.triangular_mesh(test_x[:, 0], test_x[:, 1],
-                     test_x[:, 2], faces, scalars=posterior_sample)
+mlab.figure('One Posterior Sample', size=(1920, 1360), fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
+mlab.triangular_mesh(sampled_x[:, 0], sampled_x[:, 1], sampled_x[:, 2], faces, scalars=posterior_sample)
 mlab.colorbar(orientation='vertical')
+mlab.view(0.0, 180.0, 0.5139171204775793)
 
 # Kernel evaluation
 kernel_eval = kernel_eval - np.min(kernel_eval)
 kernel_eval /= np.max(kernel_eval)
-mlab.figure('Kernel Evaluation', size=(1920, 1360),
-            fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
-mlab.triangular_mesh(test_x[:, 0], test_x[:, 1],
-                     test_x[:, 2], faces, scalars=kernel_eval)
+mlab.figure('Kernel Evaluation', size=(1920, 1360), fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
+mlab.triangular_mesh(sampled_x[:, 0], sampled_x[:, 1], sampled_x[:, 2], faces, scalars=kernel_eval)
 mlab.colorbar(orientation='vertical')
-mlab.points3d(test_x[point, 0], test_x[point, 1],
-              test_x[point, 2], **v_options)
+mlab.points3d(sampled_x[point, 0], sampled_x[point, 1], sampled_x[point, 2], **v_options)
 mlab.view(0.0, 180.0, 0.5139171204775793)
 
 # Eigenfunction
 eigfun = eigfunctions[mode, :] - np.min(eigfunctions[mode, :])
 eigfun /= np.max(eigfun)
-mlab.figure('Eigenfunction', ize=(1920, 1360),
-            fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
-mlab.triangular_mesh(test_x[:, 0], test_x[:, 1],
-                     test_x[:, 2], faces, scalars=eigfun)
+mlab.figure('Eigenfunction', size=(1920, 1360), fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
+mlab.triangular_mesh(sampled_x[:, 0], sampled_x[:, 1], sampled_x[:, 2], faces, scalars=eigfun)
 mlab.colorbar(orientation='vertical')
 mlab.view(0.0, 180.0, 0.5139171204775793)
 
 # Eigenvectors
 eigvec = eigenvector - np.min(eigenvector)
 eigvec /= np.max(eigvec)
-mlab.figure('Eigenfunction', size=(1920, 1360),
-            fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
-mlab.triangular_mesh(sampled_x[:, 0], sampled_x[:, 1], sampled_x[:, 2],
-                     faces, scalars=eigvec)
+mlab.figure('Eigenvector', size=(1920, 1360), fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
+mlab.triangular_mesh(sampled_x[:, 0], sampled_x[:, 1], sampled_x[:, 2], faces, scalars=eigvec)
 mlab.colorbar(orientation='vertical')
 mlab.view(0.0, 180.0, 0.5139171204775793)
--- tests/supervised_learning_2D.py
+++ tests/supervised_learning_2D.py
@@ -1,18 +1,25 @@
+# !/usr/bin/env python
+# encoding: utf-8
+
 import numpy as np
 import torch
 import gpytorch
-import matplotlib.pyplot as plt
+
 from mayavi import mlab
 from importlib.resources import files
+
 from manifold_gp.kernels.riemann_matern_kernel import RiemannMaternKernel
 from manifold_gp.models.riemann_gp import RiemannGP
+
 from manifold_gp.utils.generate_truth import groundtruth_from_mesh
 
+
+# Set device
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
 
+# Load mesh and generate ground truth
 data_path = files('manifold_gp.data').joinpath('dragon10k.stl')
-# data_path = files('manifold_gp.data').joinpath('dragon100k.msh')
 nodes, faces, truth = groundtruth_from_mesh(data_path)
 sampled_x = torch.from_numpy(nodes).float().to(device)
 sampled_y = torch.from_numpy(truth).float().to(device)
@@ -23,9 +30,9 @@ sampled_y = torch.from_numpy(truth).float().to(device)
 # sampled_x.sub_(mu).div_(std)
 
 # Noisy dataset
-manifold_noise = 0.00
+manifold_noise = 0.0
 noisy_x = sampled_x + manifold_noise * torch.randn(m, n).to(device)
-function_noise = 0.00
+function_noise = 0.0
 noisy_y = sampled_y + function_noise * torch.randn(m).to(device)
 
 # Train dataset
@@ -41,146 +48,106 @@ test_x = noisy_x[test_idx, :]
 test_y = noisy_y[test_idx]
 
 # Initialize kernel
-nu = 3
-neighbors = 20
+nu = 2
+neighbors = 10
 modes = 50
-kernel = gpytorch.kernels.ScaleKernel(RiemannMaternKernel(
-    nu=nu, nodes=noisy_x, neighbors=neighbors, modes=modes))
+kernel = gpytorch.kernels.ScaleKernel(RiemannMaternKernel(nu=nu, nodes=train_x, neighbors=neighbors, modes=modes))
 
 # Initialize likelihood and model
-likelihood = gpytorch.likelihoods.GaussianLikelihood(
-    noise_constraint=gpytorch.constraints.GreaterThan(1e-8))
+likelihood = gpytorch.likelihoods.GaussianLikelihood(noise_constraint=gpytorch.constraints.GreaterThan(1e-8))
 model = RiemannGP(train_x, train_y, likelihood, kernel).to(device)
 
 # Model Hyperparameters
 hypers = {
     'likelihood.noise_covar.noise': 1e-5,
-    'covar_module.base_kernel.epsilon': 0.5027,
-    'covar_module.base_kernel.lengthscale': 0.5054,
+    'covar_module.base_kernel.epsilon': 0.5,
+    'covar_module.base_kernel.lengthscale': 0.5,
     'covar_module.outputscale': 1.0,
 }
 model.initialize(**hypers)
 
 # Train model
-lr = 1e-1
+lr = 1e-2
 iters = 100
 verbose = True
 loss = model.manifold_informed_train(lr, iters, verbose)
 
+# Model Evaluation
 likelihood.eval()
 model.eval()
 
 with torch.no_grad(), gpytorch.settings.fast_pred_var():
+    # GP posteriors
     preds = likelihood(model(noisy_x))
-    mean = preds.mean
-    std = preds.stddev
-    posterior_sample = preds.sample()
-    point = 3000
-    kernel_eval = kernel(sampled_x[point, :].unsqueeze(
-        0), sampled_x).evaluate().squeeze()
-    var = kernel(sampled_x, sampled_x).diag()
-    eigfunctions = kernel.base_kernel.eigenfunctions(sampled_x).cpu().numpy()
+    mean = preds.mean.cpu().numpy()
+    std = preds.stddev.cpu().numpy()
+    posterior_sample = preds.sample().cpu().numpy()
 
-    # Bring data to cpu
+    # Prior Variance
+    var = kernel.base_kernel.variance(noisy_x).cpu().numpy()
+
+    # Kernel evaluation
+    point = 3405
+    kernel_eval = kernel(noisy_x[point, :].unsqueeze(0), noisy_x).evaluate().squeeze().cpu().numpy()
+
+    # Eigenfunctions
+    mode = 1
+    eigfunctions = kernel.base_kernel.eigenfunctions(noisy_x).cpu().numpy()
+
+    # Bring data to numpy
     sampled_x = sampled_x.cpu().numpy()
     sampled_y = sampled_y.cpu().numpy()
     train_x = train_x.cpu().numpy()
-    test_x = test_x.cpu().numpy()
-    kernel_eval = kernel_eval.cpu().numpy()
-    posterior_sample = posterior_sample.cpu().numpy()
-    mean = mean.cpu().numpy()
-    std = std.cpu().numpy()
-    var = var.cpu().numpy()
 
-# v_options = {'mode': 'sphere', 'scale_factor': 3e-3, 'color': (0, 0, 0)}
-# # mlab.figure(size=(1920, 1360), fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
-# # mlab.points3d(train_x[:, 0], train_x[:, 1], train_x[:, 2], **v_options)
-# # v_options = {'mode': 'sphere', 'scale_factor': 3e-3, 'color': (1, 0, 0)}
-# # mlab.points3d(test_x[:, 0], test_x[:, 1], test_x[:, 2], **v_options)
+v_options = {'mode': 'sphere', 'scale_factor': 3e-3, 'color': (0, 0, 0)}
+# Ground Truth
+mlab.figure('Groud Truth', size=(1920, 1360), fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
+mlab.triangular_mesh(sampled_x[:, 0], sampled_x[:, 1], sampled_x[:, 2], faces, scalars=sampled_y)
+mlab.colorbar(orientation='vertical')
+mlab.view(0.0, 180.0, 0.5139171204775793)
+mlab.points3d(train_x[:, 0], train_x[:, 1], train_x[:, 2], **v_options)
 
-# # Ground Truth
-# mlab.figure('Groud Truth', size=(1920, 1360),
-#             fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
-# mlab.triangular_mesh(sampled_x[:, 0], sampled_x[:, 1],
-#                      sampled_x[:, 2], faces, scalars=sampled_y)
-# bar = mlab.colorbar(orientation='vertical')
-# # bar.data_range = (0, 10)
-# mlab.points3d(train_x[:, 0], train_x[:, 1], train_x[:, 2], **v_options)
-# mlab.view(0.0, 180.0, 0.5139171204775793)
+# Mean
+mlab.figure('Mean', size=(1920, 1360), fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
+mlab.triangular_mesh(sampled_x[:, 0], sampled_x[:, 1], sampled_x[:, 2], faces, scalars=mean)
+mlab.colorbar(orientation='vertical')
+mlab.view(0.0, 180.0, 0.5139171204775793)
+mlab.points3d(train_x[:, 0], train_x[:, 1], train_x[:, 2], **v_options)
 
-# # Mean
-# mlab.figure('Mean', size=(1920, 1360), fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
-# mlab.triangular_mesh(sampled_x[:, 0], sampled_x[:, 1],
-#                      sampled_x[:, 2], faces, scalars=mean)
-# bar = mlab.colorbar(orientation='vertical')
-# # bar.data_range = (0, 10)
-# mlab.points3d(train_x[:, 0], train_x[:, 1], train_x[:, 2], **v_options)
-# mlab.view(0.0, 180.0, 0.5139171204775793)
+# Standard Deviation
+mlab.figure('Standard Deviation', size=(1920, 1360), fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
+mlab.triangular_mesh(sampled_x[:, 0], sampled_x[:, 1], sampled_x[:, 2], faces, scalars=std)
+mlab.colorbar(orientation='vertical')
+mlab.view(0.0, 180.0, 0.5139171204775793)
+mlab.points3d(train_x[:, 0], train_x[:, 1], train_x[:, 2], **v_options)
 
-# # Standard Deviation
-# mlab.figure('Standard Deviation', size=(1920, 1360),
-#             fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
-# mlab.triangular_mesh(sampled_x[:, 0], sampled_x[:, 1],
-#                      sampled_x[:, 2], faces, scalars=std)
-# bar = mlab.colorbar(orientation='vertical')
-# # bar.data_range = (0, 10)
-# mlab.points3d(train_x[:, 0], train_x[:, 1], train_x[:, 2], **v_options)
-# mlab.view(0.0, 180.0, 0.5139171204775793)
+# One Posterior Sample
+mlab.figure('One Posterior Sample', size=(1920, 1360), fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
+mlab.triangular_mesh(sampled_x[:, 0], sampled_x[:, 1], sampled_x[:, 2], faces, scalars=posterior_sample)
+mlab.colorbar(orientation='vertical')
+mlab.view(0.0, 180.0, 0.5139171204775793)
+mlab.points3d(train_x[:, 0], train_x[:, 1], train_x[:, 2], **v_options)
 
-# # # Posterior Sample
-# # mlab.figure(size=(1920, 1360), fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
-# # mlab.triangular_mesh(sampled_x[:, 0], sampled_x[:, 1],
-# #                      sampled_x[:, 2], faces, scalars=posterior_sample)
-# # bar = mlab.colorbar(orientation='vertical')
-# # # bar.data_range = (0, 10)
-# # mlab.points3d(train_x[:, 0], train_x[:, 1], train_x[:, 2], **v_options)
+# Prior Variance
+mlab.figure('Prior Variance', size=(1920, 1360), fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
+mlab.triangular_mesh(sampled_x[:, 0], sampled_x[:, 1], sampled_x[:, 2], faces, scalars=var)
+mlab.colorbar(orientation='vertical')
+mlab.view(0.0, 180.0, 0.5139171204775793)
+mlab.points3d(train_x[:, 0], train_x[:, 1], train_x[:, 2], **v_options)
 
+# Kernel evaluation
+kernel_eval = kernel_eval - np.min(kernel_eval)
+kernel_eval /= np.max(kernel_eval)
+mlab.figure('Kernel Evaluation', size=(1920, 1360), fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
+mlab.triangular_mesh(sampled_x[:, 0], sampled_x[:, 1], sampled_x[:, 2], faces, scalars=kernel_eval)
+mlab.colorbar(orientation='vertical')
+mlab.points3d(sampled_x[point, 0], sampled_x[point, 1], sampled_x[point, 2], **v_options)
+mlab.view(0.0, 180.0, 0.5139171204775793)
 
-# mlab.figure('Kernel Evaluation', size=(1920, 1360),
-#             fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
-# mlab.triangular_mesh(sampled_x[:, 0], sampled_x[:, 1],
-#                      sampled_x[:, 2], faces, scalars=kernel_eval)
-# mlab.colorbar(orientation='vertical')
-# mlab.points3d(sampled_x[point, 0], sampled_x[point, 1],
-#               sampled_x[point, 2], **v_options)
-
-# mlab.figure('Prior Variance', size=(1920, 1360),
-#             fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
-# mlab.triangular_mesh(sampled_x[:, 0], sampled_x[:, 1],
-#                      sampled_x[:, 2], faces, scalars=var)
-# mlab.points3d(train_x[:, 0], train_x[:, 1], train_x[:, 2], **v_options)
-# mlab.colorbar(orientation='vertical')
-
-# # Eigenfunctions
-# mode = 1
-# eigfun = eigfunctions[mode, :] - np.min(eigfunctions[mode, :])
-# eigfun /= np.max(eigfun)
-# mlab.figure('Eigenfunction', size=(1920, 1360),
-#             fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
-# mlab.triangular_mesh(sampled_x[:, 0], sampled_x[:, 1],
-#                      sampled_x[:, 2], faces, scalars=eigfun)
-# mlab.colorbar(orientation='vertical')
-# mlab.view(0.0, 180.0, 0.5139171204775793)
-
-
-# mode = 10
-# eigfun = eigfunctions[mode, :] - np.min(eigfunctions[mode, :])
-# eigfun /= np.max(eigfun)
-# mlab.figure('Eigenfunction', size=(1920, 1360),
-#             fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
-# mlab.triangular_mesh(sampled_x[:, 0], sampled_x[:, 1],
-#                      sampled_x[:, 2], faces, scalars=eigfun)
-# mlab.colorbar(orientation='vertical')
-# mlab.view(0.0, 180.0, 0.5139171204775793)
-# # mlab.savefig('dragon_eigfun1_100k.png')
-
-# # eigvec = kernel.base_kernel.eigenvectors[:, mode].cpu().numpy(
-# # ) - np.min(kernel.base_kernel.eigenvectors[:, mode].cpu().numpy())
-# # eigvec /= np.max(eigvec)
-# # mlab.figure('Eigenvector', size=(1920, 1360),
-# #             fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
-# # mlab.triangular_mesh(sampled_x[:, 0], sampled_x[:, 1], sampled_x[:, 2],
-# #                      faces, scalars=eigvec)
-# # mlab.colorbar(orientation='vertical')
-# # mlab.view(0.0, 180.0, 0.5139171204775793)
-# # # mlab.savefig('dragon_eigfun1_5k.png')
+# Eigenfunction
+eigfun = eigfunctions[mode, :] - np.min(eigfunctions[mode, :])
+eigfun /= np.max(eigfun)
+mlab.figure('Eigenfunction', size=(1920, 1360), fgcolor=(0, 0, 0), bgcolor=(1, 1, 1))
+mlab.triangular_mesh(sampled_x[:, 0], sampled_x[:, 1], sampled_x[:, 2], faces, scalars=eigfun)
+mlab.colorbar(orientation='vertical')
+mlab.view(0.0, 180.0, 0.5139171204775793)
