b95253f4ccc9189b660d0d2a0a52ae1861e6db3f
bernardo <bernardo.fichera@gmail.com>
2023-04-03 15:03:33 +0200

    Some helper to check cuda memory allocation.

---
+++ manifold_gp/utils/torch_helper.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import torch
+
+
+def memory_allocation():
+    import gc
+    for obj in gc.get_objects():
+        try:
+            if torch.is_tensor(obj) or (hasattr(obj, 'data') and torch.is_tensor(obj.data)):
+                print(type(obj), obj.size())
+        except:
+            pass
